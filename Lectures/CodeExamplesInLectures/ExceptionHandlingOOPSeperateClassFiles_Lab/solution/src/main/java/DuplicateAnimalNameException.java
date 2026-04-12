84997088a45cdd412ecad06226fcc388fc45cfc2
azharhanif <83354175+azharhanif@users.noreply.github.com>
2026-04-12 11:22:01 -0400

    Add files via upload

---
+++ Lectures/CodeExamplesInLectures/ExceptionHandlingOOPSeperateClassFiles_Lab/solution/src/main/java/DuplicateAnimalNameException.java
@@ -0,0 +1,6 @@
+
+public class DuplicateAnimalNameException extends Exception {
+    public DuplicateAnimalNameException(String message) {
+        super(message);
+    }
+}
---
+++ Lectures/CodeExamplesInLectures/ExceptionHandlingOOPSeperateClassFiles_Lab/solution/src/main/java/InvalidMileageException.java
@@ -0,0 +1,6 @@
+
+public class InvalidMileageException extends Exception {
+    public InvalidMileageException(String message) {
+        super(message);
+    }
+}
