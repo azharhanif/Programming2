5858ddb589ce21f71f0b16d3dca87312ad57a367
azharhanif <83354175+azharhanif@users.noreply.github.com>
2026-05-03 10:54:32 -0400

    Delete Inclass Codes directory

--- Inclass Codes/Programming2EQ3/src/Abstract1.java
+++
@@ -1,9 +0,0 @@
-
-public class Abstract1 {
-
-	public static void main(String[] args) {
-		//Animal myAnimal = new Animal();
-
-	}
-
-}
--- Inclass Codes/Programming2EQ3/src/Dog.java
+++
@@ -1,9 +0,0 @@
-
-public class Dog extends DomesticatedAnimal {
-	String size;
-	
-	@Override
-	public void makeSound() {
-		System.out.println("Woof");
-	}
-}
